97c84ab5117270f6d8e6557fb064bd293149265a
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-29 11:33:16 -0400

    Create fucntion taht decides which operation to use

--- calc.js
+++ calc.js
@@ -14,3 +14,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+// Determine necessary operation
+function operate(a, b, operator) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(a,b);
+            break;
+        case '-':
+            result = subtract(a,b);
+            break;
+        case 'x':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}
