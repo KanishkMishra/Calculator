f1d2ab7612c816cfde166d87737fa981131ee9ef
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-29 11:25:33 -0400

    Created 4 main functions and basic html with connections to other files

---
+++ calc.js
@@ -0,0 +1,16 @@
+// Operations
+function add(a, b) {
+    return Number(a)+Number(b);
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
